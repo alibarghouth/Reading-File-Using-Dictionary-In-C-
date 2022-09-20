e259c79dbda5228f0ea6a65ca0013cda015d3162
alijbarghouth <alijbarghouth@gmail.com>
2022-09-20 21:26:38 +0300

    version One

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+using Reading_File_Using_Dictionary;
+
+namespace CountryDemoApp 
+{ 
+class Program
+{
+    private static void Main(string[] args)
+    {
+            String CsvFile = "C:\\Users\\user\\source\\repos\\Reading File Using Dictionary\\Country.Csv";
+            ReadingCsvFile readingCsvFile = new ReadingCsvFile(CsvFile);
+            Dictionary<string, Country> dictionary = new Dictionary<string, Country>();
+            Console.WriteLine("Enter Country Code");
+            String UserInput = Console.ReadLine();
+            dictionary = readingCsvFile.ReadFile();
+            bool isExist = dictionary.TryGetValue(UserInput, out Country country);
+            if (isExist)
+            {
+                Console.WriteLine($"The Country is: {country.Name} and ThePopulation is : {country.Population} ");
+            }
+            else
+            {
+                Console.WriteLine("The Country iS Not Exist");
+            }
+    }
+}
+}
---
+++ ReadingCsvFile.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reading_File_Using_Dictionary
+{
+     class ReadingCsvFile
+    {
+        private String _csvFileName;
+        public ReadingCsvFile(String _csvFileName)
+        {
+            this._csvFileName = _csvFileName;
+        }
+        
+        public Dictionary<String,Country> ReadFile()
+        {
+            Dictionary<String,Country> result = new Dictionary<String,Country>();
+
+            using(StreamReader reader = new StreamReader(this._csvFileName))
+            {
+                reader.ReadLine();
+                String line;
+                while((line = reader.ReadLine()) != null){
+                    Country country = ConvartedLineToArray(line);
+                    result.Add(country.Code.ToString(), country);
+                }
+            }
+
+            return result;
+        }
+        private Country ConvartedLineToArray(String line)
+        {
+            String[] lineArray = line.Split(',');
+            String name;
+            String Code;
+            String Region;
+            String Population;
+            switch (lineArray.Length)
+            {
+                case 4:
+                    name = lineArray[3];    
+                    Code = lineArray[2];    
+                    Region = lineArray[1];
+                    Population = lineArray[0];
+                    break;
+                case 5:
+                    name = lineArray[3] + "," + lineArray[4];
+                    name = name.Replace("\'", null).Trim();
+                    Code = lineArray[2];
+                    Region = lineArray[1];
+                    Population = lineArray[0];
+                    break;
+
+                 default:
+                    throw new Exception($"Error  {line} ");
+            };
+            int.TryParse(Population, out int population);
+            return new Country(name,Code,Region,population);
+        }
+        
+
+
+        
+    }
+}
